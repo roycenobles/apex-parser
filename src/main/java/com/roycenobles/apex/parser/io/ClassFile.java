de8d76eca768cc60358bcfef5b2e6c8b2d8efbf5
Royce Nobles <ranobles@gmail.com>
2017-07-29 16:37:28 -0400

    Updating license reference.

--- src/main/java/com/roycenobles/apex/parser/io/ClassFile.java
+++ src/main/java/com/roycenobles/apex/parser/io/ClassFile.java
@@ -1,5 +1,21 @@
+/*
+ * Licensed under MIT (https://github.com/roycenobles/apex-parser/blob/master/LICENSE)
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.roycenobles.apex.parser.io;
 
+/**
+ * Represents an Apex Class with source text and metadata.
+ *
+ * @author Royce Nobles
+ */
 public class ClassFile {
 
     String name;
