077691d525f61529240a4ef16270caa243a098f2
Royce Nobles <royce.nobles@ncino.com>
2017-08-03 12:47:26 -0400

    Building out token stream.

--- src/main/java/com/roycenobles/apex/parser/ast/parsing/TokenStream.java
+++ src/main/java/com/roycenobles/apex/parser/ast/parsing/TokenStream.java
@@ -2,6 +2,18 @@ package com.roycenobles.apex.parser.ast.parsing;
 
 public class TokenStream {
 
+    public Boolean eof() {
+        return true;
+    }
 
+    public Token next() {
+
+        return null;
+    }
+
+    public Token peek() {
+
+        return null;
+    }
 
 }
---
+++ src/test/java/com/roycenobles/apex/parser/ast/parsing/TokenStreamTest.java
@@ -0,0 +1,13 @@
+package com.roycenobles.apex.parser.ast.parsing;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TokenStreamTest {
+
+    @Test
+    public void test() throws Exception {
+
+        Assert.assertTrue(true);
+    }
+}
